d767c3f7c87684e14b4adb33aa2a997e330c96bb
Robin de Witte <r.dewitte@student.fontys.nl>
2021-09-22 14:10:07 +0200

    eerste opzet accountservice
    gegenereerd, DTO en nugets grotendeels

---
+++ fictivus_accountservice/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fictivus_accountservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
